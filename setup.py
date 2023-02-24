34111409443dd811162ee1c4c9d9fd3098952da0
Sulthan Abiyyu <zblackjack980@gmail.com>
2023-02-24 21:33:27 +0700

    fix: `setup.py` packages

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name='dflogger',
-    packages=find_packages(),
-    version='0.1.0',
+    packages=['dflogger.logger'],
+    version='0.1.1',
     description='small and dataframe based machine learning experiment logger',
     author='Sulthan Abiyyu',
     license='MIT',
